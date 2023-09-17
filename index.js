99ef942664d8bc7365789ad98a2f8397594777d5
Daphyk <kendid07gmail.com>
2023-09-18 00:33:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
 }
+const { expect } = require('chai');
+
+describe('index.js', function() {
+  it('binds an event listener in addingEventListener()', function() {
+
+    const input = document.getElementById('input');
+    
+    const clickEventListener = input.onclick; 
+    
+    expect(clickEventListener).to.equal(clickAlert);
+  });
+});
+
