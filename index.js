b186e98bf0ee62b7e9bb1880c9b7c50e6b88c8bb
Doreen Kendi Thuranira <kendid07@gmail.com>
2023-09-28 21:57:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const { expect } = require('chai');
-
-function clickAlert() {
-}
-const { addingEventListener } = require('./path-to-your-code');
-
-describe('index.js', function() {
-  it('binds an event listener in addingEventListener()', function() {
-    const input = document.createElement('input');
-    input.id = 'input';
-    input.onclick = clickAlert;
-
-    document.body.appendChild(input);
-    addingEventListener();
-
-    const clickEventListener = input.onclick;
-
-    expect(clickEventListener).to.equal(clickAlert);
-    document.body.removeChild(input);
-  });
-});
-
+function addingEventListener() {
+  const button= document.getElementById("button")
+  button.addEventListener("click", function(){
+      alert("I was clicked!")
+  } )
+} 
